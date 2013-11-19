a7e280e996bb8659d6417f534883df77f8407daa
Carl Holmberg <info@talgdank.se>
2013-11-19 07:30:20 +0100

    Added cover retrieval from LibraryThing

--- app/inc/app/Image.php
+++ app/inc/app/Image.php
@@ -14,9 +14,23 @@ class Image extends \app\Controller {
     function get($app, $params) {
         if (isset($params['id'])) {
             $file = $params['id'];
-            foreach(array('.png', '.jpg', '.jpeg') as $format) {
-                if (file_exists('data/img/'.$file.$format)) {
-                    $img = new \Image($file.$format, false, 'data/img/');
+            if (file_exists('data/img/'.$file)) {
+                $size = getimagesize('data/img/'.$file);
+                if ($size[0] > 1) {
+                    $img = new \Image($file, false, 'data/img/');
+                    $img->render();
+                    return;
+                }
+            } else {
+            
+                $url = sprintf('http://covers.librarything.com/devkey/%s
+/large/isbn/%s', $app->get('LIBDEVKEY'), $file);
+                $img = 'data/img/'.$file;
+                file_put_contents($img, file_get_contents($url));
+    
+                $size = getimagesize('data/img/'.$file);
+                if ($size[0] > 1) {
+                    $img = new \Image($file, false, 'data/img/');
                     $img->render();
                     return;
                 }
